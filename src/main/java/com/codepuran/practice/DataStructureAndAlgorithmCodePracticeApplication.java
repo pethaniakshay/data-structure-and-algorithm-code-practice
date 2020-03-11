228a8065653ec01a9961055a7206c15b04202495
Akshay Pethani <patelaksh412@gmail.com>
2020-03-11 23:47:44 +0530

    Initial Commit

---
+++ src/main/java/com/codepuran/practice/DataStructureAndAlgorithmCodePracticeApplication.java
@@ -0,0 +1,13 @@
+package com.codepuran.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataStructureAndAlgorithmCodePracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataStructureAndAlgorithmCodePracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codepuran/practice/DataStructureAndAlgorithmCodePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codepuran.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataStructureAndAlgorithmCodePracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
